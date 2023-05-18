3652d176d80488f214bafb6b2d95e8e2e2ebe071
NehaS101 <solankineha297@gmail.com>
2023-05-18 10:39:27 +0530

    added

--- Dsa/Valid Parentheses.js
+++ Dsa/Valid Parentheses.js
@@ -1,1 +1,22 @@
 //Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid
+Input: s = "()[]{}"
+Output: true
+var isValid = function(s) {
+    const stack = [];
+     let count = 0;
+     for (let i = 0; i < s.length; i++) {
+         if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+             stack.push(s[i]);
+             count++;
+         } else {
+             if (stack.length === 0) return false;
+             const ch = stack.pop();
+             if ((s[i] === ')' && ch === '(') || (s[i] === ']' && ch === '[') || (s[i] === '}' && ch === '{')) {
+             } else {
+                 return false;
+             }
+             count--;
+         }
+     }
+     return count === 0;  
+ };
