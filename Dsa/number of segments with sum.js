1b1bc2db1f9c459f82319ccbf0bcf076970773b6
NehaS101 <solankineha297@gmail.com>
2023-05-19 18:22:26 +0530

    question

---
+++ Dsa/number of segments with sum.js
@@ -0,0 +1,14 @@
+function repeating(str){
+let obj={};
+for(let key of str){
+    obj[key]? obj[key]++ : obj[key] =1;
+
+}
+for(let key in str){
+    if(obj[str[key]]==1){
+        console.log(key)
+        return;
+    }
+}
+console.log(-1)
+}
