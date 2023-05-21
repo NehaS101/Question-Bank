38805958fe53f6bde8bb6c0a0ba2598296b5fc16
NehaS101 <solankineha297@gmail.com>
2023-05-21 12:52:12 +0530

    new question

---
+++ house making.js
@@ -0,0 +1,9 @@
+function houseMakeing(N,K){
+    //write code here
+    let brick=N*K;
+    if(brick>=1000 && brick % 5 == 0){
+        console.log("yes");
+    }else{
+        console.log("no");
+    }
+}
