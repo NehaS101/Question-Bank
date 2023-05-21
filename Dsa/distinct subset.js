17808b06d3d3651585d2bc0a7447072eb758599f
NehaS101 <solankineha297@gmail.com>
2023-05-21 09:08:01 +0530

    new question

--- Dsa/distinct subset.js
+++ Dsa/distinct subset.js
@@ -1,23 +1,52 @@
-function printDistinctSubsetSums(nums) {
+
+  
+function Distinct(N,arr) {
+  // console.log(N,arr)
     const distinctSums = [];
-    const n = nums.length;
-    const totalSubsets = Math.pow(2, n);
-  
-    for (let i = 0; i < totalSubsets; i++) {
-      let sum = 0;
-      for (let j = 0; j < n; j++) {
-        if ((i & (1 << j)) !== 0) {
-          sum += nums[j];
-        }
+ 
+  const totalSubsets = Math.pow(2, N);
+
+  for (let i = 0; i < totalSubsets; i++) {
+    let sum = 0;
+    for (let j = 0; j < N; j++) {
+      if ((i & (1 << j)) !== 0) {
+        sum += arr[j];
       }
-      distinctSums.push(sum);
     }
-  
-    distinctSums.sort((a, b) => a - b);
-    console.log(distinctSums);
+    distinctSums.push(sum);
   }
-  
-  // Example usage:
-  const nums = [1, 2, 2];
-  printDistinctSubsetSums(nums);
+
+  distinctSums.sort((a, b) => a - b);
+  console.log(distinctSums.join(" "));
+}
+function runProgram(input) {
+// Write code here
+input= input.trim().split('\n')
+var N = +input[0]
+
+  var arr = input[1].trim().split(" ").map(Number)
+Distinct(N,arr)    
+}
+
+if (process.env.USER === "") {
+runProgram(``);
+} else {
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+  read += input;
+});
+process.stdin.on("end", function () {
+  read = read.replace(/\n$/, "");
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+});
+process.on("SIGINT", function () {
+  read = read.replace(/\n$/, "");
+  runProgram(read);
+  process.exit(0);
+});
+}
+
   
