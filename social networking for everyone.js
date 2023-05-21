3efe89db3f5090d229ed25ac5b2105adffee3be7
NehaS101 <solankineha297@gmail.com>
2023-05-21 12:57:36 +0530

    new question

---
+++ social networking for everyone.js
@@ -0,0 +1,16 @@
+function socialNetworking(N) {
+    // Write code here
+    if(N<13){
+        console.log("1 Kms");
+    }else  if( N>=13 && N<18){
+        
+        console.log("5 Kms");
+          
+    }else  if(N>=18 && N<30){
+     
+        console.log("10 Kms");
+     
+    }else{
+        console.log("You can have friends from anywhere");
+    }
+}
